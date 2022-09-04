005e4467f490034ec880cf19fd7570a4c6c192f8
bigfatball <95281151+bigfatball@users.noreply.github.com>
2022-09-04 18:55:45 +0800

    Add files via upload

---
+++ webserver/main.rs
@@ -0,0 +1,10 @@
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(fs::Files::new("/", "../app/dist").index_file("index.html")))
+        .bind(("192.168.65.130", 8080))?
+        .run()
+        .await
+}
